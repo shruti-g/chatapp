c574dbe249e63e772018267218bda6da75d4675c
shruti-g <shruti7323@gmail.com>
2020-05-14 20:37:08 +0530

    socket io connection

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const express=require("express");
 const  path=require('path');//no need to install as it is global package
-
+const http=require('http');
+const socketio= require('socket.io');
 
 const app=express();
+
+const server=http.createServer(app);
+const io=socketio(server)
 
 const port=process.env.PORT || 3000;
 
 const publicDirectoryPath=path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
+io.on('connection',()=>{
+  console.log("new socket connection");
+})
 
-app.listen(port,()=>{
+server.listen(port,()=>{
   console.log(`server is up and running on ${port}`);
 });
